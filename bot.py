15177c114a5ec088eb608f926165540ce04feaf0
Lonick32 <39029077+Lonick32@users.noreply.github.com>
2018-11-17 07:13:27 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import os
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+  print("Hey look, I'm on!")
+  
+@bot.command()
+async def say(msg):
+  await bot.say(msg)
+  
+  
+bot.run(os.environ['TOKEN'])
